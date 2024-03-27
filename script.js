2ff0306f3ab7b7084c804f2187f0b24481250b5a
Rhuanzinsz <162989054+Rhuanzinsz@users.noreply.github.com>
2024-03-26 23:19:18 -0300

    Montado a pagina principal e as outras guias

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Hello!');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slides = document.querySelectorAll('.slide');
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+
+//botao de proximo slide
+nextBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides - 1)) {
+        slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
+
+//botao de voltar slide
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+    slideNumber--;
+
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides - 1;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
+
+
