2d4f9972cd88aba3b7bda226d80829e56a08596e
Rhuanzinsz <162989054+Rhuanzinsz@users.noreply.github.com>
2024-03-31 01:15:26 -0300

    Trabalho finalizado
    Ultimos ajustes feitos e finalizado

---
+++ questoes.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Qual Trailer de game mais visto da historia?",
+        answer: "C. Subway Surf",
+        options: [
+            "A. Grand Theft Auto 6",
+            "B. Grand Theft Auto 5",
+            "C. Subway Surf",
+            "D. Minecraft"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual Console mais vendido na historia?",
+        answer: "A. Playstation 2",
+        options: [
+            "A. Playstation 2",
+            "B. Playstation 4",
+            "C. Nintendo Switch",
+            "D. Nintendo Wii"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual jogo mais vendido da historia?",
+        answer: "B. Minecraft",
+        options: [
+            "A. Tetris",
+            "B. Minecraft",
+            "C. Grand Theft Auto 5",
+            "D. Super Mario Bros"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Qual a maior marca de videogame do mundo?",
+        answer: "D. Sony",
+        options: [
+            "A. Nintendo",
+            "B. Microsoft",
+            "C. Riot",
+            "D. Sony"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quando foi lançado o primeiro video game?",
+        answer: "B. 1972",
+        options: [
+            "A. 1968",
+            "B. 1972",
+            "C. 1960",
+            "D. 1983"
+        ]
+    },
+];
